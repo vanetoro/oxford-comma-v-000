8db6df9b53cb743329634c79bad71ab32ef57b37
Vanessa Toro <vanetoro84@gmail.com>
2018-03-23 00:04:25 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.size == 2
+    array.join(' and ')
+  elsif array.size > 2
+    last_element = array.pop
+    new_string = array.join (', ') 
+      return "#{new_string}, and #{last_element}"
+  else 
+    array.join
+  end
 end
