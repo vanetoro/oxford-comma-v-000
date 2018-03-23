a374dee8cfc6265cc3d7a1a1850858d4e42b9f8d
Vanessa Toro <vanetoro84@gmail.com>
2018-03-23 00:02:36 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,8 @@ def oxford_comma(array)
   if array.size == 2
   array.join(' and ')
   else 
-   array.join (' , ') 
+  last_element = array.pop
+   new_string = array.join (' , ') 
+   return "#{new_string} and #{last_element}"
   end
 end
